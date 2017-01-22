5ffb655fc6f35e3bd1b9ccd1d102d3ec241dcab3
ekingan <ekingan@gmail.com>
2017-01-22 11:52:11 -0800

    spinner

---
+++ app/assets/javascripts/stocks.js
@@ -0,0 +1,23 @@
+var initStockLookup = function() {
+	$('#stock-lookup-form').on('ajax:before', function(event, data, status) {
+		showSpinner();
+	});
+	$('#stock-lookup-form').on('ajax:after', function(event, data, status) {
+		hideSpinner();
+	});
+
+	$('#stock-lookup-form').on('ajax:success', function(event, data, status){
+		$('#stock-lookup').replaceWith(data);
+		initStockLookup();
+	});
+
+	$('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error) {
+		hideSpinner();
+		$('#stock-lookup-results').replaceWith(' ');
+		$('#stock-lookup-errors').replaceWith('Stock was not found.');
+	});
+};
+
+$(document).ready(function() {
+	initStockLookup();
+});
